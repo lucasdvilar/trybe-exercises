b3809fa85270aaff5d1964e031440fa9455ab3fd
Lucas Vilar <lucasdantasvilar@hotmail.com>
2021-11-05 23:28:45 -0300

    exercise 5.

---
+++ fundamentos/bloco-04-intro-js-logica/dia-01-intro-primeiros-passos/exercicio5.js
@@ -0,0 +1,13 @@
+const angle1 = 30
+const angle2 = 100
+const angle3 = 50
+
+if (angle1 > 0 && angle2 > 0 && angle3 > 0) {
+    if ((angle1 + angle2 + angle3) === 180) {
+        console.log("true")
+    } else {
+        console.log("false")
+    }
+} else {
+    console.log("erro")
+}
