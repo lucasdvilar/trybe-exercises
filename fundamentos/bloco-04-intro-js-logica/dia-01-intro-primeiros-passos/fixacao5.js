33d335b1a8227a5a7984d7528db3e3a4f581067d
Lucas Vilar <lucasdantasvilar@hotmail.com>
2021-11-05 22:30:46 -0300

    adding two fixation exercises from logical operators content.

---
+++ fundamentos/bloco-04-intro-js-logica/dia-01-intro-primeiros-passos/fixacao5.js
@@ -0,0 +1,7 @@
+let weekDay = "DOMINGO".toLowerCase()
+
+if (weekDay === "sábado" || weekDay === "domingo") {
+    console.log("FINALMENTE, descanso merecido UwU")
+} else {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+}
