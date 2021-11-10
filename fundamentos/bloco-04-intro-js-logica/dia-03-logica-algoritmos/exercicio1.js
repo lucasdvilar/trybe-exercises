45c974ff7aa45f7dc2cd40eee09553f52989c0c4
Lucas Vilar <lucasdantasvilar@hotmail.com>
2021-11-09 21:56:31 -0300

    exercises 1 and 2.

---
+++ fundamentos/bloco-04-intro-js-logica/dia-03-logica-algoritmos/exercicio1.js
@@ -0,0 +1,10 @@
+let n = 5
+let ast = ""
+
+for (let i = 0; i < n; i += 1) {
+    ast += "*"
+}
+
+for (let i = 0; i < n; i += 1) {
+    console.log(ast)
+}
---
+++ fundamentos/bloco-04-intro-js-logica/dia-03-logica-algoritmos/exercicio2.js
@@ -0,0 +1,7 @@
+let n = 5
+let line = ""
+
+for (let i = 0; i < n; i += 1) {
+    line += "*"
+    console.log(line)
+}
