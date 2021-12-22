8bbfbe322577087e59e488fdacb8fd5fed08f237
Lucas Vilar <lucasdantasvilar@hotmail.com>
2021-12-21 23:29:08 -0300

    fixation exercise

---
+++ fundamentos/bloco-08-higher-order-functions-do-js-es6/dia-01-intro-hof/fixation.js
@@ -0,0 +1,9 @@
+const wakingUp = () => 'Acordando!!';
+
+const breakfast = () => 'Bora tomar café!!';
+
+const sleep = () => 'Partiu dormir!!';
+
+const doingThings = (thing) => console.log(thing());
+
+doingThings(wakingUp);
