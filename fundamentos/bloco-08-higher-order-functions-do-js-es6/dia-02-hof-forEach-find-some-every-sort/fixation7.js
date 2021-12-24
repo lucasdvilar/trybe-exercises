e5a45d8a022613ee04033ba8e6d5cf3231630ebe
Lucas Vilar <lucasdantasvilar@hotmail.com>
2021-12-23 21:24:42 -0300

    fixation exercises

---
+++ fundamentos/bloco-08-higher-order-functions-do-js-es6/dia-02-hof-forEach-find-some-every-sort/fixation7.js
@@ -0,0 +1,11 @@
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((person1, person2) => person1.age - person2.age);
+
+console.log(people);
