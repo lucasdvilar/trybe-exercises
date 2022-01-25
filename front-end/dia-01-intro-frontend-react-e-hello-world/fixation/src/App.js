9e3ff1c6f80014d61e65fb767017a20df80a61f7
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-01-25 01:06:57 -0300

    fixation exercise

---
+++ front-end/dia-01-intro-frontend-react-e-hello-world/fixation/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AboutMe from './Component';
+
+function App() {
+  return <AboutMe/>
+}
+
+export default App;
---
+++ front-end/dia-01-intro-frontend-react-e-hello-world/fixation/src/Component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutMe extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Lucas Vilar</h1>
+        <p>About me</p>
+      </div>
+    )
+  }
+}
+
+export default AboutMe;
