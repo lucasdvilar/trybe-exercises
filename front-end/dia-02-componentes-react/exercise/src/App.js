505b372333d4600a69ddcac755742b0f49196899
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-01-26 19:25:14 -0300

    late exercise

---
+++ front-end/dia-02-componentes-react/exercise/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Pokedex from "./Pokedex";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Pokedex/>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/dia-02-componentes-react/exercise/src/Pokedex.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Pokemon from "./Pokemon";
+import pokemons from "./data";
+
+class Pokedex extends React.Component {
+  render () {
+    return (
+      <div>
+        {pokemons.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon}/>)}
+      </div>
+    )
+  }
+}
+
+export default Pokedex;
