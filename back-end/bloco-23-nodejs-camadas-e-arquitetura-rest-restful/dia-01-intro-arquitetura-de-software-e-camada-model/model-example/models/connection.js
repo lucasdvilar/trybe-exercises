69d2b0caa621c3016cfda97e5af967bdd65571ff
Lucas Vilar <84539824+lucasdvilar@users.noreply.github.com>
2022-06-01 02:23:10 -0300

    Update connection.js

--- back-end/bloco-23-nodejs-camadas-e-arquitetura-rest-restful/dia-01-intro-arquitetura-de-software-e-camada-model/model-example/models/connection.js
+++ back-end/bloco-23-nodejs-camadas-e-arquitetura-rest-restful/dia-01-intro-arquitetura-de-software-e-camada-model/model-example/models/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
 	host: 'localhost',
 	user: 'root',
-	password: 'ldv129038',
+	password: 'password',
 	database: 'model_example' });
 
 module.exports = connection;
