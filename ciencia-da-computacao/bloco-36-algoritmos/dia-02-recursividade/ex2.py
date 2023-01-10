c8b5dc7533ee332e758c036db447a3c0fb0f28f7
Lucas Vilar <lucasdantasvilar@hotmail.com>
2023-01-09 23:48:20 -0300

    exercises

---
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-02-recursividade/ex2.py
@@ -0,0 +1,9 @@
+def conta_pares_recursivo(n):
+  if n == 1:
+    return 0
+  elif n % 2 == 0:
+    return 1 + conta_pares_recursivo(n - 1)
+  else:
+    return conta_pares_recursivo(n - 1)
+
+print(conta_pares_recursivo(10))
